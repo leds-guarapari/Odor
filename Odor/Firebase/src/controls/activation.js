598a19373f0f7f48334da161672508d4fcf59a94
Walber Beltrame <walber.beltrame@gmail.com>
2021-02-05 16:23:44 -0300

    Acerto da pagina de ativaçao

--- Odor/Firebase/src/controls/activation.js
+++ Odor/Firebase/src/controls/activation.js
@@ -1,1 +1,2 @@
 const code = new mdc.textField.MDCTextField(document.querySelector("#code"));
+const progress = new mdc.linearProgress.MDCLinearProgress(document.querySelector(".mdc-linear-progress"));
