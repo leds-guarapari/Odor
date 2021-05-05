f65ec417f61831c9d7fe2afaf4e8701cfb82f139
Walber Beltrame <walber.beltrame@gmail.com>
2021-05-05 09:29:49 -0300

    Enviar email para administradores

--- Odor/Firebase/functions/index.js
+++ Odor/Firebase/functions/index.js
@@ -2,6 +2,24 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+// Configure the email transport using the default SMTP transport and a Gmail account.
+// For Gmail, enable these:
+// 1. https://www.google.com/settings/security/lesssecureapps
+// 2. https://accounts.google.com/DisplayUnlockCaptcha
+// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
+// TODO: Configure the `gmail.email`, `gmail.password` and `gmail.to` Google Cloud environment variables.
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const gmailTo = functions.config().gmail.to;
+const mailTransport = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+  user: gmailEmail,
+  pass: gmailPassword,
+ },
+});
 
 admin.initializeApp();
 
@@ -51,4 +69,20 @@ exports.sendMessage = functions.database.ref('/odors/{odorId}').onCreate(async (
   // resolve promise
   return Promise.resolve();
  }
+});
+
+exports.sendMail = functions.database.ref('/odors/{odorId}').onCreate(async (snapshot) => {
+ // initialize odor
+ let odor = snapshot.val();
+ // initialize mail options
+ let mailOptions = {
+  from: `Odor <${gmailEmail}>`,
+  to: gmailTo,
+  subject: odor.Type,
+  text: odor.Address,
+ };
+ // send mail
+ await mailTransport.sendMail(mailOptions);
+ // resolve promise
+ return Promise.resolve();
 });
